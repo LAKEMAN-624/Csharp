81d202d64a9c3ba873349728a474b659454dd610
LAKEMAN-624 <zhangyuhan0624@163.com>
2019-09-30 15:42:59 +0800

    改个名

---
+++ Csharp/WindowsFormsHelloWorld1/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsHelloWorld1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textshowhello_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void buttonsayhello_Click(object sender, EventArgs e)
+        {
+            textshowhello.Text = "Hello World!";
+        }
+    }
+}
