d0727ac35ba5cd941a98da9c7b2a06e3d3de3ece
LAKEMAN-624 <zhangyuhan0624@163.com>
2019-10-01 10:41:19 +0800

    调用DLL类库 项目引用

---
+++ Csharp/HelloWorld1001/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Tools;
+
+namespace HelloWorld1001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double result = Calculator.Div(1,0);
+            Console.WriteLine(result);
+        }
+    }
+}
