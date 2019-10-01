6607e5b77a1f5a5e1c1f839f37c48cbb0300ff06
LAKEMAN-624 <zhangyuhan0624@163.com>
2019-10-01 19:38:42 +0800

    类与对象；变量与实例

---
+++ Class4/Class4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Classandinstance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Form myForm1;
+            Form myForm2;
+            myForm1 = new Form();
+            myForm2 = myForm1;
+            myForm1.Text = "My Form!";
+            myForm2.Text = "Change!";
+            myForm1.ShowDialog();
+            myForm2.ShowDialog();
+        }
+    }
+}
