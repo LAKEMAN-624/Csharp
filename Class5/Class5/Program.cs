11453893a8d75d0160aff7fec55972f5a8d5e128
LAKEMAN-624 <zhangyuhan0624@163.com>
2019-10-02 21:55:16 +0800

    class 5 初识类

---
+++ Class5/Class5/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Calculator c = new Calculator();
+          //  int x = c.Add(1, 14);
+            //Console.WriteLine(x);
+           Calculator d = new Calculator();
+            //string y = d.GetToday();
+            //Console.WriteLine(y);
+            d.PrintSum(1, 3);
+        }
+        class Calculator
+        {
+            public int Add(int a, int b)
+            {
+                int result = a + b;
+                return result;
+            }
+            public string GetToday()
+            {
+                int day = DateTime.Now.Day;
+                return day.ToString();
+            }
+            public void PrintSum(int a, int b)
+            {
+                int result = a + b;
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
---
+++ Class5/method/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator c = new Calculator();
+            int result = c.SumAll(100);
+            Console.WriteLine(result);
+        }
+    }
+    class Calculator
+    {
+
+        //public int printxto1(int x)       
+        //{
+        //int s = 0;
+        //    for (int i = x; i > 0; i--)
+        //    {
+        //        s = s + i;
+        //    }
+        //    return s;
+        //}
+
+
+        public int SumAll(int x)
+        {
+            if (x == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                int result = x + SumAll(x - 1);
+                return result;
+            }
+        }
+    }
+}
