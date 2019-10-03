a108bff40418aea8b0ab8c44709cc627a9e6611f
LAKEMAN-624 <zhangyuhan0624@163.com>
2019-10-03 10:37:23 +0800

    Class5汉诺塔

---
+++ Class6/Class6/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace Class5_hannuota
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Trans c = new Trans();
+            double  result = c.Move(64);
+            Console.WriteLine(result);
+            double standard = Math.Pow(2, 64);
+            Console.WriteLine(standard-1);
+        }
+    }
+    class Trans
+    {
+        public double Move(int x)
+        {
+            if (x == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                double result = 2 * Move(x - 1) + 1;
+                return result;
+            }
+        }
+    }
+}
