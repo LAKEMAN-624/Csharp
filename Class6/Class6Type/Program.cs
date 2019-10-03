d393d96136dc7c58c0c96190e48d9ad1a0f8e162
LAKEMAN-624 <zhangyuhan0624@163.com>
2019-10-03 11:04:15 +0800

    强类型弱类型

---
+++ Class6/Class6Type/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Class6Type
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            x = 100;
+            long y;
+            y = 100L;
+            x = 100L;
+
+            bool b;
+            b = false;
+            b = 100;
+        }
+    }
+}
