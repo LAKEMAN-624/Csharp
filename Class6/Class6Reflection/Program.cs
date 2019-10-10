92d6823b49978797e50dc72578b388b5339bdc75
LAKEMAN-624 <zhangyuhan0624@163.com>
2019-10-10 10:40:57 +0800

    Class6 Reflection

---
+++ Class6/Class6Reflection/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Reflection.Context;
+namespace Class6Reflection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Type myType = typeof(Form);
+            
+            Console.WriteLine(myType.BaseType.FullName);
+        }
+    }
+}
