372b2cb8faa5baceb1bd82f420d534caa4e47f50
LAKEMAN-624 <zhangyuhan0624@163.com>
2019-10-12 16:28:30 +0800

    overload

---
+++ Class8method/Class8Constructor/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class8Constructor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student stu = new Student();
+            Console.WriteLine(stu.ID);
+        }
+    }
+
+    class Student
+    {
+        public Student()
+        {
+            this.ID = 1;
+            this.Name = "No Name";
+        }
+        public int ID;
+        public string Name;
+    }
+}
---
+++ Class8method/Overload/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Overload
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    class Calculator
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+        public double Add(double a, double b)
+        {
+            return a + b;
+        }
+    }
+}
