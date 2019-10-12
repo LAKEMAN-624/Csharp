07aee140227891863ee89443b11a13a1daf30513
LAKEMAN-624 <zhangyuhan0624@163.com>
2019-10-12 15:39:21 +0800

    method 方法的由来，声明与调用

---
+++ Class8method/Class8method/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class8method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator c = new Calculator();
+            double Volume = c.GetConeVolume(5,5);
+            Console.WriteLine(Volume);
+        }
+    }
+    class Calculator
+    {
+        public double GetCircleArea(double r)
+        {
+            return Math.PI * r * r;
+        }
+        public double GetCylinderVolume(double r, double h)
+        {
+            return GetCircleArea(r) * h;
+        }
+        public double GetConeVolume(double r, double h)
+        {
+            return GetCylinderVolume(r, h) / 3;
+        }
+    }
+}
