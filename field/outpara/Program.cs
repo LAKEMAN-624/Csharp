9890b1cd32d011cf6c4b36d852e036d47cd19623
LAKEMAN-624 <zhangyuhan0624@163.com>
2019-10-16 02:42:19 +0800

    传值参数，输出参数，引用参数，数组参数

---
+++ field/outpara/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace outpara
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = 10;
+            bool b = DoubleParser.TryParse("ABC", out x);
+            if(b==true)
+            {
+                Console.WriteLine(x + 1);
+            }
+            else
+            {
+                Console.WriteLine(x);
+            }
+
+        }
+    }
+    class DoubleParser
+    {
+        public static bool TryParse(string input,out double result)
+        {
+            try
+            {
+                result = double.Parse(input);
+                return true;
+            }
+            catch 
+            {
+                result = 0;
+                return false;
+            }
+        }
+    }
+}
---
+++ field/refpara/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace refpara
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int y = 1;
+            IWandSideEffect(ref y);
+            Console.WriteLine(y);
+        }
+        static void IWandSideEffect(ref int x)
+        {
+            x = x + 100;
+        }
+    }
+}
