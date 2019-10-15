7b45aff6392b9d6c0ee1d9a938b0acc6a13e830f
LAKEMAN-624 <zhangyuhan0624@163.com>
2019-10-15 18:34:31 +0800

    property

---
+++ field/property/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace property
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Student stu1 = new Student();
+                stu1.SetAge(20);
+                Student stu2 = new Student();
+                stu2.SetAge(20);
+                Student stu3 = new Student();
+                stu3.SetAge(200);
+
+                int Average = (stu1.GetAge() + stu2.GetAge() + stu3.GetAge()) / 3;
+                Console.WriteLine(Average);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
+        }
+    }
+    class Student
+    {
+        private int age;
+        public int GetAge()
+        {
+            return this.age;
+        }
+        public void SetAge(int value)
+        {
+            if(value>= 0 && value<=120)
+            {
+                this.age = value;
+            }
+            else
+            {
+                throw new Exception("Age value has error");
+            }
+        }
+    }
+}
