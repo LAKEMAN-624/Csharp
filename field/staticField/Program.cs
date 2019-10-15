48b877a2ece3e70ff7758b38a297b5ac698f1307
LAKEMAN-624 <zhangyuhan0624@163.com>
2019-10-15 16:49:35 +0800

    static field readonly

---
+++ field/staticField/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace staticField
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(brush.DefaultColor.Red);
+            Console.WriteLine(brush.DefaultColor.Green);
+            Console.WriteLine(brush.DefaultColor.Blue);
+        }
+    }
+    struct Color
+    {
+        public int Red;
+        public int Green;
+        public int Blue;
+    }
+    class brush
+    {
+        public static readonly Color DefaultColor = new Color() { Red = 0, Green = 0, Blue = 0 };
+    }
+}
