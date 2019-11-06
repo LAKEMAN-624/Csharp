d6bffa4d7f9a43db3adfcd61b714d24d6e1544ae
LAKEMAN-624 <zhangyuhan0624@163.com>
2019-11-06 22:15:21 +0800

    Class internal  public

---
+++ Class/Class/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyLib.MyNameSpace;
+namespace Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator cal = new Calculator();
+            double res = cal.Add(1, 1);
+            Console.WriteLine(res);
+        }
+    }
+}
